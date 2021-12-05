b7ff4a8f3692e9ccd9f687b16b719de32e81a3b1
EffenrilAgung <66568136+EffenrilAgung@users.noreply.github.com>
2021-12-06 04:56:38 +0700

    Foods

---
+++ script.js
@@ -0,0 +1,20 @@
+// const ICONS = document.querySelectorAll('.section-1-icons i')
+// let i = 1
+// setInterval(()=> {
+//     i++
+
+//     const ICON = document.querySelector('.section-1-icons .change')
+    
+//     ICON.classList.remove('change')
+//     if(i > ICONS.length) {
+//         ICONS[0].classList.add('change')
+//         i = 1
+//     }else {
+//         ICON.nextElementSibling.classList.add('change')
+//     }
+// }, 1000)
+
+let hobbies = ["Sport", "Code"]
+for(let hobbie of hobbies) {
+    console.log(hobbie)
+}
